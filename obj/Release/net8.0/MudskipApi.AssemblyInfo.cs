f6139df347176649615d9c901aa10b37c5dc2a9b
nagymatekaroly <nagymatekaroly@gmail.com>
2025-04-15 15:58:14 +0200

    last push 2

--- obj/Release/net8.0/MudskipApi.AssemblyInfo.cs
+++ obj/Release/net8.0/MudskipApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MudskipApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a638b4d434dccdadb073409475ebdb58a835f018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a153afa2e166f84cdef4a8ddea969bcf64baf69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MudskipApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MudskipApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
